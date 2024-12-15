39e4d47f63e9c5143224b8a4d71bb2610ddf26a5
ziparh <thelackutop@gmail.com>
2024-12-15 15:54:19 +0100

    add naming conventions

--- app/core/option.py
+++ app/core/option.py
@@ -13,6 +13,14 @@ class DataBaseSettings(BaseModel):
     echo: bool = True
     echo_pool: bool = True
 
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
 
 
 class Settings(BaseSettings):
