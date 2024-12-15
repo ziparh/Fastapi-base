d4448c9907df4bb4a3de4462e1f4de0ae1a12acd
ziparh <thelackutop@gmail.com>
2024-12-15 15:23:21 +0100

    Add Base model

---
+++ app/core/db/base.py
@@ -0,0 +1,14 @@
+from sqlalchemy import MetaData
+from sqlalchemy.orm import DeclarativeBase, declared_attr
+
+from app.core import settings
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    metadata = MetaData(naming_convention=settings.db.naming_convention)
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return f'{cls.__name__.lower()}s'
